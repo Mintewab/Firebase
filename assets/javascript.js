f36bb751f0dcc2e3149808fcfdc7c34c62d45729
Mintewab <demissiewminte123@gmail.com>
2019-05-11 22:50:43 -0700

    make sure that javascript tag is linked and add firebase reference

---
+++ assets/javascript.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCrfmpDkMCyDh-IXTLs3zX3WVkHqeWIfGc",
+    authDomain: "train-time-home-work.firebaseapp.com",
+    databaseURL: "https://train-time-home-work.firebaseio.com",
+    projectId: "train-time-home-work",
+    storageBucket: "train-time-home-work.appspot.com",
+    messagingSenderId: "870341012134",
+    appId: "1:870341012134:web:4f0c30ce4966f442"
+};
