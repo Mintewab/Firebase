eb07bdefd61ae31b3ea562c3e8aafbe14d2e678f
Mintewab <demissiewminte123@gmail.com>
2019-05-27 23:28:45 -0700

    modified java script

--- assets/javascript.js
+++ assets/javascript.js
@@ -25,30 +25,53 @@ var nextArrival = moment().add(minutesAway, "minutes");
 $("#submit").on("click", function (event) {
     event.preventDefault();
 
-    trainName = $("#train-name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    time = $("#time-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var time = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
 
     var newPostRef = trainRef.push({
         "trainName": trainName,
         "destination": destination,
         "frequency": frequency,
+        "time": time,
         "minutesAway": minutesAway,
         "nextArrival": nextArrival.format("HH:mm").toString()
     });
-    var childRow = '<tr class="train-data-child"> <td class="train-name">' + trainName + '</td> <td class="destination">' + destination + '</td> <td class="frequency">' + frequency + '</td> <td class="next-arrival">' + nextArrival + '</td> <td class="minutes-away">' + minutesAway + '</td></tr > '
-
-    $("#train-data").append(childRow);
 });
 
 trainRef.on('child_added', function (data) {
+    var trainInformation = data.val();
+    var frequency = trainInformation.frequency;
+    var destination = trainInformation.destination;
+    var time = trainInformation.time
+    var trainName = trainInformation.trainName;
+    var firstTimeConverted = moment(time, "HH:mm");
+    var nextArrival;
+    var minutesAway;
+    var latestTime = moment.max(moment(), firstTimeConverted);
+
+    if (firstTimeConverted === latestTime) {
+        nextArrival = firstTimeConverted.format("HH:mm");
+        minutesAway = moment(firstTimeConverted).diff(moment(), "minutes");
+    } else {
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        var tRemainder = diffTime % frequency;
+        minutesAway = frequency - tRemainder;
+        nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
+    }
+
+    var childRow = '<tr class="train-data-child"> <td class="train-name">' + trainName
+        + '</td> <td class="destination">' + destination
+        + '</td> <td class="frequency">' + frequency
+        + '</td> <td class="next-arrival">' + nextArrival
+        + '</td> <td class="minutes-away">' + minutesAway + '</td></tr >';
+    $("#train-data").append(childRow);
 
 });
 
 firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
+       var errorCode = error.code;
     var errorMessage = error.message;
-    // ...
+
 });
